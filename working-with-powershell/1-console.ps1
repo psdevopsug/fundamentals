09119508bdaf167f1e26fcadf9a6b57e2cbd8e0b
Paul Broadwith <paul@pauby.com>
2018-02-12 22:38:34 +0000

    Updated

--- working-with-powershell/1-console.ps1
+++ working-with-powershell/1-console.ps1
@@ -30,3 +30,45 @@ $profile
 #===
 
 # Create a PowerShell profile file
+$source = Join-Path -Path $env:USERPROFILE -ChildPath "repo\fundamentals\working-with-powershell\1-Microsoft.PowerShell_profile.ps1"
+$dest = Join-Path -Path $env:USERPROFILE -ChildPath "Documents\WindowsPowerShell\Microsoft.PowerShell_Profile.ps1"
+Copy-Item -Path $source -Destination $dest
+
+# Some of the module we need are not installed
+# Lets see what's imported into the session
+Get-Module
+
+# Find out what modules are installed
+Get-Module -ListAvailable
+
+# Find out if our missing modules are in the list
+$missingModules = @('posh-git', 'posh-with', 'jump.location', 'pstodotxt')
+Get-Module -ListAvailable | Where { $_.name -in $missingModules }
+
+# Check that the modules are in the PowerShell Gallery
+Find-Module -Name $missingModules
+
+# Install the modules from the PowerShell Gallery
+Install-Module -Name $missingModules -Scope CurrentUser -Verbose
+
+# Trust the PowerShell Gallery
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# Install the modules from the PowerShell Gallery
+Install-Module -Name $missingModules -Scope CurrentUser -Verbose
+
+# Start a new session and see it load correctly
+
+# Remove posh-with module
+Uninstall-Module -Name 'posh-with' -Verbose
+
+# PowerShell will automatically load amodule if you use one of it's functions /
+# cmdlets
+# But if you want to import a module manually
+Import-Module -Name 'posh-git' -Verbose
+
+# ... and remove it manually
+Remove-Module -Name 'posh-git' -Verbose
+
+# If you want to find out functions or cmdlets are inside a module
+Get-Command -Module 'posh-git'
